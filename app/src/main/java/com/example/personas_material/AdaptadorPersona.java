5e18b271f35cbbd084c222b68d4ab84063f3cd9c
Jose Arevalo <job.38011@gmail.com>
2020-05-30 08:32:37 -0500

    Adaptaor Persona funcional

--- app/src/main/java/com/example/personas_material/AdaptadorPersona.java
+++ app/src/main/java/com/example/personas_material/AdaptadorPersona.java
@@ -1,5 +1,6 @@
 package com.example.personas_material;
 
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -12,47 +13,51 @@ import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
-public class AdaptadorPersona {
+    public class AdaptadorPersona extends RecyclerView.Adapter<AdaptadorPersona.PersonaViewHolder>{
+        private ArrayList<Persona> personas;
 
-    public static class PersonaViewHolder extends RecyclerView.Adapter<AdaptadorPersona.PersonaViewHolder>{
-    private ArrayList<Persona> personas;
-
-    public AdaptadorPersona(ArrayList<Persona> personas){
-        this.personas = personas;
-    }
+        public AdaptadorPersona(ArrayList<Persona> personas) {
+            this.personas = personas;
+        }
 
 
         @NonNull
-        @Override
         public PersonaViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-            return null;
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_persona, parent, false);
+            return new PersonaViewHolder(v);
         }
 
         @Override
         public void onBindViewHolder(@NonNull PersonaViewHolder holder, int position) {
-
+            Persona p = personas.get(position);
+            holder.foto.setImageResource(p.getFoto());
+            holder.cedula.setText(p.getCedula());
+            holder.nombre.setText(p.getNombre());
+            holder.apellido.setText(p.getApellido());
         }
 
         @Override
         public int getItemCount() {
-            return 0;
+            return personas.size();
         }
 
-        private ImageView foto;
-        private TextView cedula;
-        private TextView nombre;
-        private TextView apellido;
-        private View v;
+        public static class PersonaViewHolder extends RecyclerView.ViewHolder{
+            private ImageView foto;
+            private TextView cedula;
+            private TextView nombre;
+            private TextView apellido;
+            private View v;
 
-        public PersonaViewHolder(View itemView){
-            super(itemView);
-            v = itemView;
-            foto = v.findViewById(R.id.imgFoto);
-            cedula = v.findViewById(R.id.lblCedula);
-            nombre = v.findViewById(R.id.lblNombre);
-            apellido = v.findViewById(R.id.lblApellido);
+            public PersonaViewHolder(View itemView) {
+                super(itemView);
+                v = itemView;
+                foto = v.findViewById(R.id.imgFoto);
+                cedula = v.findViewById(R.id.lblCedula);
+                nombre = v.findViewById(R.id.lblNombre);
+                apellido = v.findViewById(R.id.lblApellido);
+            }
         }
 
 
     }
-}
+
